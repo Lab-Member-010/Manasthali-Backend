5bddbbbb100581f9d15304db546f5e7f3187857e
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-12 23:48:51 +0530

    socket.io app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
+import http from 'http';
+import { Server } from 'socket.io';  
 import bodyParser from "body-parser";
 import badgeRouter from "./routes/badge.route.js";
 import commentRouter from "./routes/comment.route.js";
@@ -14,8 +16,11 @@ import quizRouter from "./routes/quiz.route.js";
 import storyRouter from "./routes/story.route.js";
 import userRouter from "./routes/user.route.js"; 
 import challangesRoute from "./routes/challengesFile.route.js";
+import messageRouter from './routes/message.route.js';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);  
 
 mongoose.connect("mongodb://127.0.0.1:27017/mitraDb")
   .then(() => {
@@ -35,10 +40,24 @@ mongoose.connect("mongodb://127.0.0.1:27017/mitraDb")
     app.use("/story", storyRouter);
     app.use("/badges", badgeRouter);
     app.use("/users", userRouter);
-    app.use("/challange",challangesRoute);
+    app.use("/challange", challangesRoute);
+    app.use('/messages', messageRouter);
 
-    app.listen(3001, () => {
-        console.log("Server Started....");
+    io.on('connection', (socket) => {
+      console.log('A user connected');
+      
+      // Listen for new messages and broadcast them
+      socket.on('send-message', (data) => {
+        io.to(data.receiverId).emit('receive-message', data);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('A user disconnected');
+      });
+    });
+
+    server.listen(3001, () => {
+      console.log('Server started...');
     });
 }).catch(err => {
     console.log(err);
