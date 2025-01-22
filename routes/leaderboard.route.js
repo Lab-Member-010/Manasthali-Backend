236f907b89e656ca0bc571eab65e5018dcbfe21e
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-23 00:31:30 +0530

    new updates

--- routes/leaderboard.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getCurrentLeaderboard, getLeaderboardByType } from '../controller/leaderboard.controller.js';
-
-const router = express.Router();
-
-router.get('/leaderboard', getCurrentLeaderboard);
-router.get('/leaderboard/:type', getLeaderboardByType);
-
-export default router;
