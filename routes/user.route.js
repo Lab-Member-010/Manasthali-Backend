2af4005b05d843c802c16499a22e92883adc7706
mahima <deshmukhmahima40@gmail.com>
2025-01-24 01:38:51 +0530

    get all user

--- routes/user.route.js
+++ routes/user.route.js
@@ -14,7 +14,7 @@ import {
   unfollowUser,
   verifyOtp,
   forgotPassword,
-  resetPassword
+  resetPassword,getAllUsersExceptOne
 } from "../controller/user.controller.js";
 import { body } from "express-validator";
 import { auth } from "../middleware/auth.js";
@@ -57,6 +57,10 @@ router.post("/reset-password",
 
 // Get user details by ID
 router.get("/:id", auth, getUserById);
+
+//Get all user
+// router.get("/viewuser",auth,getalluser)
+router.get("/get-all-users-except/:id",auth,getAllUsersExceptOne);
 
 // Update user details
 router.put("/:id", auth,upload.single("profile_picture"), updateUserById);
