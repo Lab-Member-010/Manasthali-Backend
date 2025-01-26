8a52e2864a732f2e1d816c1a538e5ee903718de0
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-26 12:43:29 +0530

    badges and challengesFile model and controller updates

--- routes/badge.route.js
+++ routes/badge.route.js
@@ -3,7 +3,7 @@ import { getUserBadges, getAllBadges } from '../controller/badge.controller.js';
 
 const router = express.Router();
 
-router.get('/badges/:userId', getUserBadges);
-router.get('/badges', getAllBadges);
+router.get('/badges-all/:userId', getUserBadges);
+router.get('/badge-today/:userId', getTodayBadge);
 
 export default router;
