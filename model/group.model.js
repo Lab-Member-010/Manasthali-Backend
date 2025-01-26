6532bdcf125569a6f38150bd7bf094099ed52437
SwastiBakliwal <bakliwalswasti@gmail.com>
2025-01-26 20:08:38 +0530

    group update

--- model/group.model.js
+++ model/group.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 
+// GroupMessage Schema
 const groupMessageSchema = new mongoose.Schema(
   {
     sender: {
@@ -30,16 +31,19 @@ const groupMessageSchema = new mongoose.Schema(
   }
 );
 
+// Group Schema with `groupIcon` added
 const groupSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: String,
   created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  messages: [groupMessageSchema],
-  communityId: { type: mongoose.Schema.Types.ObjectId, ref: 'Community' }
+  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'GroupMessage' }],  // Reference to GroupMessage model
+  communityId: { type: mongoose.Schema.Types.ObjectId, ref: 'Community' },
+  groupIcon: { type: String, default: '' },  // Add groupIcon field for the image URL or path
 }, {
   timestamps: true
 });
 
+// Create models
 export const Group = mongoose.model('Group', groupSchema);
 export default mongoose.model('GroupMessage', groupMessageSchema);
