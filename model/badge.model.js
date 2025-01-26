fb0ae79004668ff23094745ef8cfcdde52559914
Lab-Member-010 <jayeshsharmalab@gmail.com>
2025-01-26 14:31:14 +0530

    random

--- model/badge.model.js
+++ model/badge.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const badgeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: {type:String, required:false},
+  description: {type:String, required:true},
   icon: {type:String, required:false},
   criteria: {type:String, required:false}
 }, {
